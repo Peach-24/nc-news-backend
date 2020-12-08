af5049c85a332601d0ec305cab04a10e1c0f14cf
Josh Peachey <joshpeachey24@gmail.com>
2020-12-08 20:53:54 +0000

    Amended migration file for image test for articles

--- db/migrations/20201109115246_create_table_articles_table.js
+++ db/migrations/20201109115246_create_table_articles_table.js
@@ -1,15 +1,16 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("articles", (articlesTable) => {
-    articlesTable.increments("article_id").primary();
-    articlesTable.text("title").notNullable();
-    articlesTable.text("body").notNullable();
-    articlesTable.integer("votes").default(0);
-    articlesTable.text("topic").references("topics");
-    articlesTable.text("author").references("users.username");
-    articlesTable.timestamp("created_at").defaultTo(knex.fn.now());
+  return knex.schema.createTable('articles', (articlesTable) => {
+    articlesTable.increments('article_id').primary();
+    articlesTable.text('title').notNullable();
+    articlesTable.text('body').notNullable();
+    articlesTable.integer('votes').default(0);
+    articlesTable.text('img');
+    articlesTable.text('topic').references('topics');
+    articlesTable.text('author').references('users.username');
+    articlesTable.timestamp('created_at').defaultTo(knex.fn.now());
   });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("articles");
+  return knex.schema.dropTable('articles');
 };
